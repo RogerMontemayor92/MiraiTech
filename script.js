062837314ddba6bd88420371219b7adca4ac4cc6
RogerMontemayor92 <roger.montemayorr92@gmail.com>
2025-05-29 17:03:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     function changeSlide() {
         images.forEach((img, i) => {
             img.style.display = i === index ? "block" : "none"; // Muestra solo la imagen actual
+            img.style.opacity = i === index ? "1" : "0"; // Hace la transición más fluida
         });
         index = (index + 1) % images.length;
     }
